151f3bd995d3bbb681afada07dc23ab084f49199
Belevskij Sergeij <bbelevskij@gmail.com>
2014-07-01 10:15:54 +0600

    Update renamer.rb

--- renamer.rb
+++ renamer.rb
@@ -1,4 +1,4 @@
-`ruby
+```ruby
 Dir.entries(Dir.pwd).each do |filename|   #=> [".", "..", "config.h", "main.rb"]
  if filename[ARGV[0]]
   newfilename = filename.gsub(ARGV[0], ARGV[1])
@@ -6,3 +6,4 @@ Dir.entries(Dir.pwd).each do |filename|   #=> [".", "..", "config.h", "main.rb"]
   puts "file #{filename} was renamed to #{newfilename}"
  end
 end
+```
